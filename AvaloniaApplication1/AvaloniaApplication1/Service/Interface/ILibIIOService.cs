d983a03d91f9a36b0f40e2fa336ec824d1e6235e
ksj10111011@naver.com <ksj10111011@naver.com>
2025-08-20 16:57:25 +0900

    feat : libiio 테스트중

---
+++ AvaloniaApplication1/AvaloniaApplication1/Service/Interface/ILibIIOService.cs
@@ -0,0 +1,16 @@
+﻿using AvaloniaApplication1.DataType;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AvaloniaApplication1.Service.Interface
+{
+    public interface ILibIIOService
+    {
+        public void start();
+        public void stop();
+        public void command<T>() where T : MyLibiioDeviceType;
+    }
+}
